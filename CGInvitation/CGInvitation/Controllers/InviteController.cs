15917722487fcf17972a1c70d97e34896fc4ca86
Monika Bisla <mobisla@microsoft.com>
2018-10-31 12:17:03 -0700

    Added simple view for accept invite.

--- CGInvitation/CGInvitation/Controllers/InviteController.cs
+++ CGInvitation/CGInvitation/Controllers/InviteController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CGInvitation.Controllers
 {
     public class InviteController : Controller
     {
+        private static string InviteCompleted = @"You are registered for Consumer Groups Dogfood.";
+
         // GET: AcceptInvite
-        public string Accept()
+        public ActionResult Accept()
         {
-            return "You are registered for Consumer Group Dogfood";
+            ViewBag.InviteCompletedText = InviteController.InviteCompleted;
+            return View();
         }
     }
 }
