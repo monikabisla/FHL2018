3d97b6487b7a9f3af7a83b17380ac3e72085d84d
Monika Bisla <mobisla@microsoft.com>
2018-10-31 11:29:18 -0700

    Created Invite Controller.

--- CGInvitation/CGInvitation/App_Start/RouteConfig.cs
+++ CGInvitation/CGInvitation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CGInvitation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Invite", action = "Accept", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CGInvitation/CGInvitation/Controllers/InviteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CGInvitation.Controllers
+{
+    public class InviteController : Controller
+    {
+        // GET: AcceptInvite
+        public string Accept()
+        {
+            return "You are registered for Consumer Group Dogfood";
+        }
+    }
+}
